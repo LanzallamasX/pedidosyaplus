1a244cdd2827301ff432ecb11b4a13ac2a2d986a
psine <patricio.sine@gmail.com>
2024-09-10 17:30:44 -0300

    update font color fix

--- src/app/components/Faq/Faq.jsx
+++ src/app/components/Faq/Faq.jsx
@@ -30,7 +30,7 @@ const Faq = () => {
   return (
     <div className={` container mx-auto px-5  my-6`}>
 
-        <h2 className="font-26 textaHeavy text-center font-semibold my-12">Preguntas frecuentes</h2>
+        <h2 className="font-26 textaHeavy text-black text-center font-semibold my-12">Preguntas frecuentes</h2>
 
 
         <Accordion />
