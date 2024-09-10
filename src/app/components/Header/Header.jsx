191c3d90030daf03b4306eb39c601bdd3ecd7487
psine <patricio.sine@gmail.com>
2024-09-10 15:24:18 -0300

    update header Mobile

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -10,26 +10,28 @@ const Header = () => {
     <>
     <div className={`bg-[#090017] text-white ${styles.gradientBg} px-5`}>
 
-        <div className='container mx-auto flex gap-4 px-5'>
+        <div className='container mx-auto flex flex-col md:flex-row gap-4 px-5'>
 
-            <div className='w-1/2 justify-around items-start flex gap-8 flex-col'>
-                <div className=''>
-                    <h1 className='textaBlack font-48 text-white xl:w-2/3 pb-6'>
+            <div className='w-full md:w-1/2 justify-around items-center md:items-start flex gap-8 flex-col'>
+                <div className='flex flex-col items-center md:items-start'>
+                    <h1 className='textaBlack text-center md:text-left font-48 text-white xl:w-2/3 pb-6'>
                         Envíos gratis ilimitados y descuentos exclusivos en miles de locales    
                     </h1>
                     <BgPromo >
                         por <span className='font-32 textaBlackItalic'>$3990</span> al mes
                     </BgPromo>
                 </div>
-                <div>
+                <div className='hidden md:block'>
                     <MainButton />
                 </div>
             </div>
 
-            <div className='w-1/2 flex justify-center relative'>
+            <div className='w-full md:w-1/2 flex justify-center relative'>
                 <Image className='absolute w-10/12  ' src="/images/trama.svg" alt="Uruguay" width={489} height={550} />
-                <Image className='z-10' src="/images/girlHero.png" alt="girl" width={489} height={550} />
-
+                <Image className='z-10 w-4/5 md:w-[489px]' src="/images/girlHero.png" alt="girl" width={489} height={550} />
+                <div className='block md:hidden absolute bottom-10 z-50'>
+                    <MainButton />
+                </div>
             </div>
 
         </div>
--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 export default function NavBar() {
   return (
     <nav className="bg-[#100423] text-white py-12 ">
-      <div className="container mx-auto flex justify-between items-center px-5">
+      <div className="container mx-auto flex justify-center md:justify-between items-center px-5">
         
         <div className="flex items-center">
            <Image src="/images/pedidosyaplus.svg" alt="Pedidos Ya Plus" width={212} height={27} />
         </div>
 
-        <div className="flex items-center space-x-6 textaRegular font-18">
+        <div className="hidden md:flex items-center space-x-6 textaRegular font-18">
           <a href="#" className="hover:text-gray-400">BENEFICIOS</a>
           <a href="#" className="hover:text-gray-400">PREGUNTAS FRECUENTES</a>
           <a href="#" className="hover:text-gray-400">SUSCRIBITE</a>
